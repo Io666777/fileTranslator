0ecd3ae71380944d9f41b6cb5a8233dce4bb86b2
Io666777 <dimaskitihin@gmail.com>
2025-10-27 12:34:35 +0300

    добавлени минимальной go логики.

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "html/template"
+)
+
+func page(w http.ResponseWriter, r *http.Request) {
+    tmpl, err := template.ParseFiles("/index.html")
+    if err != nil {
+        http.Error(w, "Ошибка шаблона", http.StatusInternalServerError)
+        return
+    }
+    tmpl.Execute(w, nil)
+}
+
+func handleRequest() {
+    http.HandleFunc("/", page)
+    fmt.Println("Сервер запущен на http://localhost:5500")
+    http.ListenAndServe(":5500", nil)
+}
+
+func main() {
+    handleRequest()
+ 
+}
