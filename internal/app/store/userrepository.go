6e0ed248fb3accaf32245c724cf2ac2cd645687c
Io666777 <dimaskitihin@gmail.com>
2025-11-25 11:50:53 +0300

    шифрование пороля+тесты

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -3,29 +3,33 @@ package store
 import "github.com/Io666777/fileTranslator/internal/app/model"
 
 type UserRepository struct {
-    store *Store
+	store *Store
 }
 
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
-    if err := r.store.db.QueryRow(
-        "INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
-        u.Email,
-        u.EncryptedPassword,
-    ).Scan(&u.ID); err != nil {
-        return nil, err
-    }
+	if err := u.BeforeCreate(); err != nil {
+		return nil, err
+	}
+    
+	if err := r.store.db.QueryRow(
+		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
+		u.Email,
+		u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
+		return nil, err
+	}
 
-    return u, nil
+	return u, nil
 }
 
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
-    u := &model.User{}
-    if err := r.store.db.QueryRow(
-        "SELECT id, email, encrypted_password FROM users WHERE email = $1",
-        email,
-    ).Scan(&u.ID, &u.Email, &u.EncryptedPassword); err != nil {
-        return nil, err
-    }
+	u := &model.User{}
+	if err := r.store.db.QueryRow(
+		"SELECT id, email, encrypted_password FROM users WHERE email = $1",
+		email,
+	).Scan(&u.ID, &u.Email, &u.EncryptedPassword); err != nil {
+		return nil, err
+	}
 
-    return u, nil
+	return u, nil
 }
--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -14,16 +14,13 @@ func TestUserRepository_Create(t *testing.T) {
 	s, teardown := store.NewTestStore(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email:             "user@example.org",
-		EncryptedPassword: "password",
-	})
+	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	databaseURL := "postgres://postgres:password@localhost:5432/ft_test?sslmode=disable" // Добавьте эту строку
+	databaseURL := "postgres://postgres:password@localhost:5432/ft_test?sslmode=disable"
 	s, teardown := store.NewTestStore(t, databaseURL)
 	defer teardown("users")
 
@@ -31,11 +28,12 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	s.User().Create(&model.User{
-		Email:             "user@example.org",
-		EncryptedPassword: "password",
-	})
-	u, err := s.User().FindByEmail(email)
+	u := model.TestUser(t)
+	u.Email = email
+	_, err = s.User().Create(u)
+	assert.NoError(t, err)
+
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
