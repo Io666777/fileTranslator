139e82bdaf0f5c21667098c9e481f4e340219046
Io666777 <dimaskitihin@gmail.com>
2025-11-29 17:43:29 +0300

    ПЕРЕЖЕЛКА НОМЕР 4

--- internal/app/store/store.go
+++
@@ -1,7 +0,0 @@
-package store
-
-type Store interface {
-	User() UserRepository
-	File() FileRepository
-	Translation() TranslationRepository
-}
--- internal/app/translator/translator.go
+++
@@ -1,8 +0,0 @@
-// internal/app/translator/translator.go
-package translator
-
-type Translator interface {
-    Translate(text, sourceLang, targetLang string) (string, error)
-    SupportsFileType(mimeType string) bool
-    TranslateFile(filePath, sourceLang, targetLang string) (string, error)
-}
